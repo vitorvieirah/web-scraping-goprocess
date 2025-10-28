11680c6a16b6499cd10c178d6de96f3d81f46c9d
VitorVieira <vitorhvvieira@gmail.com>
2025-10-27 22:24:41 -0300

    Ajuste nas configurações do banco de dados

--- src/config/database.py
+++ src/config/database.py
@@ -2,7 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from src.config.settings import DATABASE_URL
 
-engine = create_engine(DATABASE_URL, future=True)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={
+        "sslmode": "require",  # obrigatório no Supabase
+        "options": "-4"        # força IPv4 em alguns ambientes
+    },
+    future=True
+)
+
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
